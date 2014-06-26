4b7e2027432f5de00f839e53cbd1cac256762983
Yan Olshevskyy <yanolsh@bu.edu>
2014-06-26 18:50:02 -0400

    lab3

--- chap6.py
+++ chap6.py
@@ -3,7 +3,51 @@
 
 
 # Ex. 6.6
+'''
+def first(word): return word[0] //returns the first letter from string
+def last(word): return word[-1]	//returns last letter of string
+def middle(word): return word[1:-1] //returns middle letters of the string
+'''
+'''
+def first(word):
+    return word[0]
+
+
+def last(word):
+    return word[-1]
+
+
+def middle(word):
+    return word[1:-1]
+
+
+def is_palindrome(word):
+    if len(word) <= 1:
+        return True
+    if first(word) != last(word):
+        return False
+    return is_palindrome(middle(word))
+
+
+print is_palindrome('meet')
+print is_palindrome('bob')
+print is_palindrome('otto')
+print is_palindrome('redivider')
+'''
+
 
 
 
 # Ex 6.8
+
+def gcd(a,b):
+	if  isinstance(a, str) or  isinstance(b,str):
+		print "a and b be should not be string"
+
+	if b==0:
+		return a;
+	else:
+		r=a%b
+		return gcd(b,r)
+
+
--- chap7.py
+++ chap7.py
@@ -3,7 +3,34 @@
 
 
 # Ex. 7.5
+import math
 
 
+def fact(k):
+    
+    if k == 0:
+        return 1
+    else:
+        rec = fact(k-1)
+        result = k * rec
+        return result
 
+def estimate_pi():
+
+    t = 0
+    k = 0
+    factor = 2 * math.sqrt(2) / 9801
+    while True:
+        num = fact(4*k) * (1103 + 26390*k)
+        den = fact(k)**4 * 396**(4*k)
+        term = factor * num / den
+        t += term
+        
+        if abs(term) < 1e-15: break
+        k += 1
+    print k
+    return 1 / t
+
+print estimate_pi()
 # How many iterations does it take to converge?
+# it takes 2 iterations.
